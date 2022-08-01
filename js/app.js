409bc3e3a2b7f445a96d2f51e10493c6c287b0d0
Eduardo Malherbi Martins <emalherbi@gmail.com>
2022-08-01 12:54:44 -0300

    auto-generated commit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
-Reveal.initialize({
-  controls: true,
-  progress: true,
-  history: true,
-  center: true,
-  plugins: [RevealMarkdown, RevealHighlight, RevealNotes, RevealMath.KaTeX],
+$(() => {
+  console.log("jQuery methods go here...");
+
+  Reveal.initialize({
+    controls: true,
+    progress: true,
+    history: true,
+    center: true,
+    plugins: [RevealMarkdown, RevealHighlight, RevealNotes, RevealMath.KaTeX],
+  });
+
+  Reveal.on("slidechanged", (event) => {
+    // console.log(event);
+    // event.previousSlide, event.currentSlide, event.indexh, event.indexv
+  });
 });
