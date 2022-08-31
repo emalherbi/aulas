e81bdfd012326776d71831683c30cee7516cd195
Eduardo Malherbi Martins <emalherbi@gmail.com>
2022-08-31 14:42:39 -0300

    continua

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,33 @@
 module.exports = function (grunt) {
-  "use strict";
+  'use strict';
   grunt.initConfig({
-    pkg: grunt.file.readJSON("package.json"),
-    "gh-pages": {
-      options: {
-        base: "src/",
-        message: "auto-generated commit",
+    pkg: grunt.file.readJSON('package.json'),
+    properties: grunt.file.readJSON('properties.json'),
+    clean: {
+      build: ['<%= properties.dist %>'],
+    },
+    copy: {
+      html: {
+        files: [
+          { expand: true, dot: true, cwd: '<%= properties.src %>', dest: '<%= properties.dist %>', src: ['**'] },
+          { expand: true, dot: true, cwd: '<%= properties.src %>', dest: '<%= properties.dist %>/sobre-professor', src: 'index.html' },
+          { expand: true, dot: true, cwd: '<%= properties.src %>', dest: '<%= properties.dist %>/sobre-professor', src: 'index.html' },
+          { expand: true, dot: true, cwd: '<%= properties.src %>', dest: '<%= properties.dist %>/sobre-professor', src: 'index.html' },
+        ],
       },
-      src: ["**/*"],
+    },
+    'gh-pages': {
+      options: {
+        base: '<%= properties.dist %>',
+        message: 'auto-generated commit',
+      },
+      src: ['**/*'],
     },
   });
-  for (var key in grunt.file.readJSON("package.json").devDependencies) {
-    if (key !== "grunt" && key.indexOf("grunt") === 0) {
+  for (var key in grunt.file.readJSON('package.json').devDependencies) {
+    if (key !== 'grunt' && key.indexOf('grunt') === 0) {
       grunt.loadNpmTasks(key);
     }
   }
-  grunt.registerTask("default", ["gh-pages"]);
+  grunt.registerTask('default', ['clean', 'copy']);
 };
